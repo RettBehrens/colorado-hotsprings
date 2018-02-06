be0801ce05d1ad552e37a4d7adcfcc568d443258
RettBehrens <everettbehrens@gmail.com>
2018-02-05 17:34:19 -0700

    changed theme color and collapse works now

--- client/src/app/app-navbar/app-navbar.component.ts
+++ client/src/app/app-navbar/app-navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppNavbarComponent implements OnInit {
 
+  public navbarCollapsed = true;
+
   constructor() { }
 
   ngOnInit() {
